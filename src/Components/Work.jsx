5a4d38bb081c47d22ad8d7428ac9d929f7e35387
SARB <mrsinghbusiness05@gmail.com>
2023-09-03 19:06:16 +0530

    minor changes - desgining done

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -10,52 +10,58 @@ const Work = () => {
 
                   <div className = 'portfolio-container'>
                    
-                     <div className="workcard">
-                       <div className="card-img">
-                          <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
-                       </div>
-                       <div className="main-header">
-                           Insight-Ecommerce-Store
-                       </div>
-                       <div className="card-detail">
-                              An Online Store to Buy Everything  
-                              <div className="btn-section">
-                                <button className='btn'> Live Project  </button>
-                                <button> Source Code  </button>
-                              </div>
-                       </div>
+                    <div className="workcard-container">
+                        <div className="workcard">
+                          <div className="card-img">
+                              <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
+                          </div>
+                          <div className="main-header">
+                              Insight-Ecommerce-Store
+                          </div>
+                          <div className="card-detail">
+                                  An Online Store to Buy Everything  
+                                  <div className="btn-section">
+                                    <button className='btn'> Live Project  </button>
+                                    <button> Source Code  </button>
+                                  </div>
+                          </div>
+                        </div>
                      </div>
 
-                     <div className="workcard">
-                       <div className="card-img">
-                          <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
-                       </div>
-                       <div className="main-header">
-                       Insight-Social-App
-                       </div>
-                       <div className="card-detail">
-                             Connect With Friends to Share and Like Posts
-                             <div className="btn-section">
-                              <button  className='btn'> Live Project  </button>
-                              <button> Source Code  </button>
-                             </div>
-                       </div>
-                     </div>
+                     <div className="workcard-container">
+                        <div className="workcard">
+                          <div className="card-img">
+                              <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
+                          </div>
+                          <div className="main-header">
+                          Insight-Social-App
+                          </div>
+                          <div className="card-detail">
+                                Connect With Friends to Share and Like Posts
+                                <div className="btn-section">
+                                  <button  className='btn'> Live Project  </button>
+                                  <button> Source Code  </button>
+                                </div>
+                          </div>
+                        </div>
+                      </div>
 
-                     <div className="workcard">
-                       <div className="card-img">
-                          <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
-                       </div>
-                       <div className="main-header">
-                           Insight-Ecommerce-Store
-                       </div>
-                       <div className="card-detail">
-                              An Online Store to Buy Everything  
-                              <div className="btn-section">
-                                <button  className='btn'> Live Project  </button>
-                                <button> Source Code  </button>
-                              </div>
-                       </div>
+                     <div className="workcard-container">
+                        <div className="workcard">
+                          <div className="card-img">
+                              <img src = "/src/assets/web-designer.png"   alt = "Web-Designer" />
+                          </div>
+                          <div className="main-header">
+                              Insight-Ecommerce-Store
+                          </div>
+                          <div className="card-detail">
+                                  An Online Store to Buy Everything  
+                                  <div className="btn-section">
+                                    <button  className='btn'> Live Project  </button>
+                                    <button> Source Code  </button>
+                                  </div>
+                          </div>
+                        </div>
                      </div>
                      
                   </div>
