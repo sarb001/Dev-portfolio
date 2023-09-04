6dfef432c2820c5de19e0445569093934609ebd9
SARB <mrsinghbusiness05@gmail.com>
2023-09-04 15:18:00 +0530

    portfolio  completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Navbar.css' ;
 
 const Navbar = () => {
+
+   const [toggle,setToggle] = useState(false);
+
+   const ToggleMenu = () => {
+     setToggle((prev) => !prev);
+   }
+
   return (
     <div className = 'navbar-container'>
         <div className = "navtitle">
             <a id = "home" href = "#home">  iamsarb_001  </a>
         </div>
 
-        <div className = "menu-bars">
-          <a href = "#" className = "burger-menu">
+          <span onClick={ToggleMenu} className = "burger-menu">
               <span className = "bar"> </span>
               <span className = "bar"> </span>
               <span className = "bar"> </span>
-          </a>
-        </div>
+          </span>
 
-        <div className = "navlinks">
+        <div className = {`navlinks ${toggle ? 'active' : '' }`}>
             <ul>
                     <li> <a href = "#about" > About  </a>  </li>
                     <li> <a href = "#contact"  >  Contact  </a>  </li>
                     <li> <a href = "#skills" > Skills  </a> </li>
                     <li> <a href = "#work" > Work </a>  </li>
-                    <li>  <a href = "" > Resume </a> </li>
+                    <li> <a href = "" > Resume </a> </li>
             </ul>
         </div>
 
